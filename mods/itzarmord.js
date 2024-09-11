24385afff35c9e3cbb60605f2c4d4b96b21f8b87
SpectrazonSchool <145609554+SpectrazonSchool@users.noreply.github.com>
2024-09-10 19:22:57 -0500

    Update itzarmord.js

--- mods/itzarmord.js
+++ mods/itzarmord.js
@@ -3,8 +3,11 @@ elements.teleport = {
     behavior: behaviors.WALL,
     category: "special",
     tick: function(pixel){
-        pixel.ex = 1
-        pixel.why = 1
-        tryMove(pixel, ex, why)
+        var rand1 = Math.floor(Math.random() * 166);
+        var rand2 = Math.floor(Math.random() * 84);
+        pixel.ex = rand1
+        pixel.why = rand2
+        
+        tryMove(pixel, pixel.ex, pixel.why)
         }
   }
